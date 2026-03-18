c4c2d27d740cb1498128672fcc3401e4f01f0eca
Amanda Miranda <amandaoliveirabcc@gmail.com>
2026-03-18 20:03:36 -0300

    Commit De Amanda OUTRO

--- teste/src/Main.java
+++ teste/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
 
-            //Amanda TRabalhando na branch dela Commit A
+            //Amanda TRabalhando na branch dela Commit A, Amanda Continua trabalhando aqui....
         }
 
         // Coleguinha Trabalhou e Commitou - B
