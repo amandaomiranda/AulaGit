f6ecedda13438b9b2ec73a01b8a3eba25fb25ae3
Amanda Miranda <amandaoliveirabcc@gmail.com>
2026-03-18 20:03:02 -0300

    Update comment in Main.java for clarity

--- teste/src/Main.java
+++ teste/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
 
-            //Amanda TRabalhando na branch dela Commit A
+            //Amanda TRabalhando na branch dela Commit A, Colega mexeu aquui
         }
 
         // Coleguinha Trabalhou e Commitou - B
